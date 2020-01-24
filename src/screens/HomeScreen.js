c72974ed3de6f502c0a4aecc6e3b1df53fba97b5
thenatural86 <philip.smalls86@gmail.com>
2020-01-23 19:16:36 -0500

    navigation added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,21 @@
 import React from "react"
-import { Text, StyleSheet } from "react-native"
+import { Text, StyleSheet, View, Button } from "react-native"
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen; Whats good with ya</Text>
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>HomeScreen</Text>
+      <Button
+        onPress={() => navigation.navigate("Components")}
+        title="Go to Components Demo"
+      />
+
+      <Button
+        onPress={() => navigation.navigate("List")}
+        title="Go to List Demo"
+      />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
